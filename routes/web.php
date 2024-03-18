be9227f4f2e42501e734fa84ce2cfb53c7e6f93e
Benjamin Sanchez <sanchez7888555@gmail.com>
2024-03-17 20:36:34 -0600

    feat: ultimate version updated.

--- routes/web.php
+++ routes/web.php
@@ -55,6 +55,9 @@ Route::middleware('auth')->group(function () {
     Route::resource('/attendance', AttendanceController::class)->except(['edit', 'update']);
     Route::get('/attendance/{class_id}/register', [AttendanceController::class, 'register'])->name('attendance.register');
     Route::post('/attendance/{class_id}/register', [AttendanceController::class, 'addRegister'])->name('attendance.addRegister');
+    Route::get('/attendance/{class_id}/select-date', [AttendanceController::class, 'selectDatetoEdit'])->name('attendance.selectDate');
+    Route::patch('/attendance/{class_id}', [AttendanceController::class, 'update'])->name('attendance.update');
+    Route::get('/attendance/{class_id}/edit', [AttendanceController::class, 'edit'])->name('attendance.edit');
     Route::resource('/add-homeworks-score', addHomeworkScoreController::class)->except(['store', 'edit']);
     Route::get('/add-homeworks-score/{homework_id}/{assigment_id}', [addHomeworkScoreController::class, 'homeworkRegister'])->name('homework.register');
     Route::post('/add-homeworks-score/{homework_id}/{assigment_id}', [addHomeworkScoreController::class, 'store'])->name('add-homeworks-score.store');
